430037285278107ff06cbf74110956820d2719dd
raykaryshyn <raykaryshyn@gmail.com>
2021-02-08 14:31:02 -0500

    Modify Generics_Lab

--- Generics_Lab/DataSetGen.java
+++ Generics_Lab/DataSetGen.java
@@ -3,15 +3,14 @@ public class DataSetGen<T extends Measurable> {
     private T maximum;
     private int count;
 
-   /**
-      Constructs an empty data set.
-   */
-   public DataSetGen()
-   {
-      sum = 0;
-      count = 0;
-      maximum = null;
-   }
+    /**
+     * Constructs an empty data set.
+     */
+    public DataSetGen() {
+        sum = 0;
+        count = 0;
+        maximum = null;
+    }
 
     /**
      * Adds a data value to the data set.
