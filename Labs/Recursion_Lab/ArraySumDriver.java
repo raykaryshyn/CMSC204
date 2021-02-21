8d47127f6897ea3953dff58723004e39567c78c8
raykaryshyn <raykaryshyn@gmail.com>
2021-02-20 21:09:06 -0500

    Modify Recursion_Lab

--- Labs/Recursion_Lab/ArraySumDriver.java
+++ Labs/Recursion_Lab/ArraySumDriver.java
@@ -1,9 +1,17 @@
+/**
+ * Program that generates the sum of values in array and the nth number in the
+ * fibonacci sequence
+ * 
+ * @author Raymond Karyshyn
+ */
+
 public class ArraySumDriver {
 	private final static int ARRAY_SIZE = 6;
+	private static long[] fibTable = new long[10000];
 
 	public static void main(String[] args) {
+		// Array Sum (Driver)
 		int index = 0;
-
 		Integer[] myArray = new Integer[ARRAY_SIZE];
 
 		myArray[index++] = 13;
@@ -19,9 +27,17 @@ public class ArraySumDriver {
 
 		sum = sumOfArray(myArray, 5);
 		System.out.println(sum);
+		// END - Array Sum (Driver)
 
 		// Fibonacci (Driver)
-		System.out.println(fibonacci(10));
+		System.out.println("\nFIBONACCI SEQUENCE");
+		System.out.println("5th term:\t" + fibonacci(5));
+		System.out.println("10th term:\t" + fibonacci(10));
+		System.out.println("15th term:\t" + fibonacci(15));
+		System.out.println("20th term:\t" + fibonacci(20));
+		System.out.println("50th term:\t" + fibonacci(50));
+		System.out.println("100th term:\t" + fibonacci(100));
+		// END - Fibonacci (Driver)
 	}
 
 	/**
@@ -38,18 +54,22 @@ public class ArraySumDriver {
 		return (arr[num] + sumOfArray(arr, num - 1));
 	}
 
-	// Fibonacci (Recursive & Dynamic Programming)
-	private static long[] table = new long[10000];
-
+	/**
+	 * Recursive & dynamic programming method for generating the nth number in the
+	 * fibonacci sequence
+	 * 
+	 * @param n term to generate in the fibonacci sequence
+	 * @return nth term in the fibonacci sequence
+	 */
 	public static long fibonacci(int n) {
-		if (table[n] != 0)
-			return table[n];
+		if (fibTable[n] != 0)
+			return fibTable[n];
 		else if (n <= 2)
 			return 1;
 
 		long first = fibonacci(n - 1);
 		long second = fibonacci(n - 2);
-		table[n] = first + second;
-		return table[n];
+		fibTable[n] = first + second;
+		return fibTable[n];
 	}
 }
