6a0b609c4a4c7624dfef665da1daeec5d57b817b
raykaryshyn <raykaryshyn@gmail.com>
2021-03-02 16:25:32 -0500

    Modify Iteration_Lab

--- Labs/Iteration_Lab/SolitaireMatchingGame.java
+++ Labs/Iteration_Lab/SolitaireMatchingGame.java
@@ -4,58 +4,50 @@ import java.util.ListIterator;
 
 public class SolitaireMatchingGame {
     public static void main(String[] args) {
-        for (int j = 0; j < 100; j++) {
-            int passes = 0;
+        int passes = 0, pairsRemoved = 0;
 
-            final int MAX_SIZE = 20;
-            ArrayList<Integer> list = new ArrayList<Integer>(MAX_SIZE);
-            for (int i = 0; i < MAX_SIZE; i++)
-                list.add(i, 0);
-            ListIterator<Integer> iterator = list.listIterator();
-            Random random = new Random();
+        final int MAX_SIZE = 20;
+        ArrayList<Integer> list = new ArrayList<Integer>(MAX_SIZE);
+        for (int i = 0; i < MAX_SIZE; i++)
+            list.add(i, 0);
 
-            while (iterator.hasNext()) {
-                iterator.next();
-                iterator.remove();
-                iterator.add(random.nextInt(90) + 10);
-            }
-
-            System.out.println(list);
-
-            boolean removedPair = true;
-            while (removedPair) {
-                removedPair = false;
-                ListIterator<Integer> iterator2 = list.listIterator();
-                while (iterator2.hasNext()) {
-                    int next = iterator2.next();
-                    if (iterator2.hasNext()) {
-                        int next2 = iterator2.next();
-                        if ((next / 10 == next2 / 10) || (next % 10 == next2 % 10)) {
-                            /* System.out.println(next + " " + next2); */
-                            iterator2.remove();
-                            iterator2.previous();
-                            iterator2.remove();
-                            removedPair = true;
-                        } else {
-                            iterator2.previous();
-                        }
-                    }
-                }
-
-                if (removedPair) passes++;
-            }
-
-            System.out.println(passes);
-            System.out.println(list);
-            System.out.println();
+        ListIterator<Integer> iterator = list.listIterator();
+        Random random = new Random();
+        while (iterator.hasNext()) {
+            iterator.next();
+            iterator.remove();
+            iterator.add(random.nextInt(90) + 10);
         }
+
+        System.out.println("Beginning list: " + list);
+
+        boolean removedPair = true;
+        while (removedPair) {
+            removedPair = false;
+            
+            ListIterator<Integer> iterator2 = list.listIterator();
+            while (iterator2.hasNext()) {
+                int next = iterator2.next();
+                if (iterator2.hasNext()) {
+                    int next2 = iterator2.next();
+                    if ((next / 10 == next2 / 10) || (next % 10 == next2 % 10)) {
+                        iterator2.remove();
+                        iterator2.previous();
+                        iterator2.remove();
+                        removedPair = true;
+                        pairsRemoved++;
+                    } else
+                        iterator2.previous();
+                }
+            }
+
+            if (removedPair)
+                passes++;
+        }
+
+        System.out.println("\nPasses through the list: " + passes);
+        System.out.println("Pairs removed: " + pairsRemoved);
+
+        System.out.println("\nEnding list: " + list);
     }
 }
-
-// [15, 56, 83, 73, 14, 64, 83, 80, 75, 24, 65, 45, 24, 75, 50, 23, 99, 24, 74, 95]
-// 4
-// [15, 23]
-
-// [20, 96, 53, 70, 34, 95, 70, 25, 61, 37, 12, 27, 60, 19, 44, 28, 17, 24, 81, 27]
-// 0
-// [20, 96, 53, 70, 34, 95, 70, 25, 61, 37, 12, 27, 60, 19, 44, 28, 17, 24, 81, 27]
