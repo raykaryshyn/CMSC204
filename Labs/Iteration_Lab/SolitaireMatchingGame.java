671cafdf16ec88812fe531c96bb1d2fcf002d9e2
raykaryshyn <raykaryshyn@gmail.com>
2021-03-02 22:20:07 -0500

    Modify Iteration_Lab

--- Labs/Iteration_Lab/SolitaireMatchingGame.java
+++ Labs/Iteration_Lab/SolitaireMatchingGame.java
@@ -8,36 +8,29 @@ public class SolitaireMatchingGame {
 
         final int MAX_SIZE = 20;
         ArrayList<Integer> list = new ArrayList<Integer>(MAX_SIZE);
-        for (int i = 0; i < MAX_SIZE; i++)
-            list.add(i, 0);
-
-        ListIterator<Integer> iterator = list.listIterator();
         Random random = new Random();
-        while (iterator.hasNext()) {
-            iterator.next();
-            iterator.remove();
-            iterator.add(random.nextInt(90) + 10);
-        }
+        for (int i = 0; i < MAX_SIZE; i++)
+            list.add(i, random.nextInt(90) + 10);
 
         System.out.println("Beginning list: " + list);
 
         boolean removedPair = true;
         while (removedPair) {
             removedPair = false;
-            
-            ListIterator<Integer> iterator2 = list.listIterator();
-            while (iterator2.hasNext()) {
-                int next = iterator2.next();
-                if (iterator2.hasNext()) {
-                    int next2 = iterator2.next();
+
+            ListIterator<Integer> iterator = list.listIterator();
+            while (iterator.hasNext()) {
+                int next = iterator.next();
+                if (iterator.hasNext()) {
+                    int next2 = iterator.next();
                     if ((next / 10 == next2 / 10) || (next % 10 == next2 % 10)) {
-                        iterator2.remove();
-                        iterator2.previous();
-                        iterator2.remove();
+                        iterator.remove();
+                        iterator.previous();
+                        iterator.remove();
                         removedPair = true;
                         pairsRemoved++;
                     } else
-                        iterator2.previous();
+                        iterator.previous();
                 }
             }
 
