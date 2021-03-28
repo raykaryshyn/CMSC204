b8fbebd040ceaa0bb343ae24a031588260d506c0
raykaryshyn <raykaryshyn@gmail.com>
2021-03-28 15:13:59 -0400

    Modify Sets-BinaryFiles_Lab

--- Labs/Sets-BinaryFiles_Lab/RandomIntegerGenerator.java
+++ Labs/Sets-BinaryFiles_Lab/RandomIntegerGenerator.java
@@ -1,105 +1,79 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Random;
 
+import java.io.PrintWriter;
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * @author Raymond Karyshyn
+ */
 public class RandomIntegerGenerator {
     public static void main(String[] args) {
-        int[] randomIntegers = generateRandomIntegers(50000, 1000, 9999, "ascii.txt", "binary.dat");
+        ArrayList<Integer> randomIntegers = generateRandomIntegers(50000, 1000, 9999, "ascii.txt", "binary.dat");
         boolean success = checkBinaryFile("binary.dat", randomIntegers);
 
-        System.out.println(success);
+        if (success)
+            System.out.println("The binary file passed the test!");
+        else
+            System.out.println("The binary file did not pass the test.");
     }
 
-    public static int[] generateRandomIntegers(int size, int min, int max, String fileName_ascii,
+    public static ArrayList<Integer> generateRandomIntegers(int size, int min, int max, String fileName_ascii,
             String fileName_binary) {
-        int[] result = new int[size];
+        ArrayList<Integer> result = new ArrayList<>();
         Random random = new Random();
 
-        for (int i = 0; i < result.length; i++)
-            result[i] = random.nextInt((max - min) + 1) + min;
+        for (int i = 0; i < size; i++)
+            result.add(i, random.nextInt((max - min) + 1) + min);
 
         // Write to the ASCII File
-        boolean fileOpened_ascii = true;
-        PrintWriter toFile_ascii = null;
-
         try {
-            toFile_ascii = new PrintWriter(fileName_ascii);
-        } catch (FileNotFoundException e) {
-            fileOpened_ascii = false;
-        }
+            PrintWriter toFile_ascii = new PrintWriter(fileName_ascii);
 
-        if (fileOpened_ascii) {
-            for (int counter = 0; counter < result.length; counter++) {
-                int line = result[counter];
-                toFile_ascii.println(line);
-            }
+            for (int counter = 0; counter < size; counter++)
+                toFile_ascii.println(result.get(counter));
 
             toFile_ascii.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
         }
 
         // Write to binary file
-        // int resultCode = 0;
-        Random generator = new Random();
-        DataOutputStream toFile_binary = null;
         try {
-            FileOutputStream fos = new FileOutputStream(fileName_binary);
-            toFile_binary = new DataOutputStream(fos);
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName_binary));
 
-            for (int counter = 0; counter < result.length; counter++) {
-                toFile_binary.writeInt(generator.nextInt());
-            } // end for
-        } catch (FileNotFoundException e) {
-            // resultCode = 1; // Error opening file
+            oos.writeObject(result);
+
+            oos.close();
         } catch (IOException e) {
-            // resultCode = 2; // Error writing file
-        } finally {
-            try {
-                if (toFile_binary != null)
-                    toFile_binary.close();
-            } catch (IOException e) {
-                // resultCode = 3; // Error closing file
-            }
-            // return resultCode;
+            System.out.println(e);
         }
 
         return result;
     }
 
-    public static boolean checkBinaryFile(String fileName, int[] randomIntegers) {
-        boolean result = false;
+    @SuppressWarnings("unchecked")
+    public static boolean checkBinaryFile(String fileName, ArrayList<Integer> randomIntegers) {
+        ArrayList<Integer> result = new ArrayList<>();
 
-        int resultCode = 0;
-        DataInputStream fromFile = null;
         try {
-            FileInputStream fis = new FileInputStream(fileName);
-            fromFile = new DataInputStream(fis);
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName));
 
-            while (true) {
-                int number = fromFile.readInt();
-                System.out.println(number);
-            } // end while
-        } catch (FileNotFoundException e) {
-            resultCode = 1; // Error opening file
-        } catch (EOFException e) {
-            // Normal occurrence since entire file is read; ignore exception
+            result = (ArrayList<Integer>) ois.readObject();
+            result.removeAll(randomIntegers);
+
+            ois.close();
         } catch (IOException e) {
-            resultCode = 2; // Error reading file
-        } finally {
-            try {
-                if (fromFile != null)
-                    fromFile.close();
-            } catch (IOException e) {
-                resultCode = 3; // Error closing file
-            }
-            // return resultCode;
+            System.out.println(e);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e);
         }
 
-        return result;
+        return result.size() == 0;
     }
 }
