f8ec7fb52c6f1be95128a34678e34c4723dff3be
raykaryshyn <raykaryshyn@gmail.com>
2021-04-22 10:10:04 -0400

    Modify Project_5: student junit

--- Projects/Project_5/MorseCodeConverter_STUDENT_Test.java
+++ Projects/Project_5/MorseCodeConverter_STUDENT_Test.java
@@ -1,5 +1,11 @@
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +16,8 @@ import org.junit.Test;
  * @author Raymond Karyshyn
  */
 public class MorseCodeConverter_STUDENT_Test {
+	File inputFile;
+
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -21,5 +29,39 @@ public class MorseCodeConverter_STUDENT_Test {
 	@Test
 	public void testPrintTree() {
 		assertEquals("h s v i f u e l r a p w j  b d x n c k y t z g q m o", MorseCodeConverter.printTree());
+	}
+
+	@Test
+	public void testConvertToEnglishString() {
+		assertEquals("sos", MorseCodeConverter.convertToEnglish("... --- ..."));
+		assertEquals("nicholas", MorseCodeConverter.convertToEnglish("-. .. -.-. .... --- .-.. .- ..."));
+		assertEquals("ray k", MorseCodeConverter.convertToEnglish(".-. .- -.-- / -.-"));
+	}
+
+	@Test
+	public void testConvertToEnglishFile() throws FileNotFoundException {
+		String test1 = "the quick brown fox jumps over the lazy dog";
+		getFile("StudentTestFile.txt");
+		String converter1 = MorseCodeConverter.convertToEnglish(inputFile);
+		assertEquals(test1, converter1);
+	}
+
+	public void getFile(String in) throws FileNotFoundException {
+		JFileChooser chooser = new JFileChooser();
+		int status;
+
+		chooser.setDialogTitle("Select Input File - " + in);
+		status = chooser.showOpenDialog(null);
+
+		if (status == JFileChooser.APPROVE_OPTION) {
+			try {
+				inputFile = chooser.getSelectedFile();
+				// readFile();
+			} catch (Exception e) {
+				// e.printStackTrace();
+				JOptionPane.showMessageDialog(null, "There is a problem with this file", "Error",
+						JOptionPane.ERROR_MESSAGE);
+			}
+		}
 	}
 }
