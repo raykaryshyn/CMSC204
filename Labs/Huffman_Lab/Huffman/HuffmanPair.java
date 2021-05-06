2962762408d0ec1f8c063f65cf472b8878c031dd
raykaryshyn <raykaryshyn@gmail.com>
2021-05-06 00:20:23 -0400

    Modify Huffman_Lab

--- Labs/Huffman_Lab/Huffman/HuffmanPair.java
+++ Labs/Huffman_Lab/Huffman/HuffmanPair.java
@@ -1,28 +1,63 @@
+/**
+ * Represents an encoding of a message with the necessary information for
+ * decoding.
+ * 
+ * @author Raymond Karyshyn
+ */
 public class HuffmanPair {
     private HuffmanTree huffmanTree;
     private String encodedMessage;
 
+    /**
+     * Constructor setting both attributes as null.
+     */
     public HuffmanPair() {
         this(null, null);
     }
 
+    /**
+     * Constructor for setting both attributes with given arguments.
+     * 
+     * @param huffmanTree    The Huffman tree used to encode the message.
+     * @param encodedMessage The encoded message.
+     */
     public HuffmanPair(HuffmanTree huffmanTree, String encodedMessage) {
         this.huffmanTree = huffmanTree;
         this.encodedMessage = encodedMessage;
     }
 
+    /**
+     * Gets the stored Huffman tree.
+     * 
+     * @return A HuffmanTree object representing the stored Huffman tree.
+     */
     public HuffmanTree getHuffmanTree() {
         return huffmanTree;
     }
 
+    /**
+     * Sets the Huffman tree.
+     * 
+     * @param huffmanTree The Huffman tree to be stored.
+     */
     public void setHuffmanTree(HuffmanTree huffmanTree) {
         this.huffmanTree = huffmanTree;
     }
 
+    /**
+     * Gets the stored encoded message.
+     * 
+     * @return A string representing the stored encoded message.
+     */
     public String getEncodedMessage() {
         return encodedMessage;
     }
 
+    /**
+     * Sets the encoded message.
+     * 
+     * @param encodedMessage The encoded message to be stored.
+     */
     public void setEncodedMessage(String encodedMessage) {
         this.encodedMessage = encodedMessage;
     }
