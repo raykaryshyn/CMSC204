73c859a55a278504b2181d7ed53636dcfadcd744
raykaryshyn <raykaryshyn@gmail.com>
2021-05-07 11:04:29 -0400

    Modify Project_6: student tests

--- Projects/Project_6/src/Town_STUDENT_Test.java
+++ Projects/Project_6/src/Town_STUDENT_Test.java
@@ -1,3 +1,47 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class Town_STUDENT_Test {
-    
+    String name;
+    Town town;
+
+    @Before
+    public void setUp() throws Exception {
+        name = "My Town";
+        town = new Town(name);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        name = null;
+        town = null;
+    }
+
+    @Test
+    public void testCompareTo() {
+        assertEquals(true, town.compareTo(new Town("My Other Town")) > 0);
+        assertEquals(true, town.compareTo(new Town("My Zesty Town")) < 0);
+        assertEquals(true, town.compareTo(new Town("My Town")) == 0);
+    }
+
+    @Test
+    public void testEquals() {
+        assertEquals(true, town.equals(new Town(name)));
+        assertEquals(false, town.equals(new Town("Town...")));
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals(name, town.getName());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(name, town.toString());
+    }
+
+    @Test
+    public void testHashCode() {
+        assertEquals(String.valueOf(name).hashCode(), town.hashCode());
+    }
 }
