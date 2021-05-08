012d36fd6e5d197c4f4234c676d86bc90ae31e27
raykaryshyn <raykaryshyn@gmail.com>
2021-05-08 10:10:45 -0400

    Modify Huffman_Lab

--- Labs/Huffman_Lab/Huffman/Driver.java
+++ Labs/Huffman_Lab/Huffman/Driver.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 /**
  * Utilizes the Huffman class to encode and decode various messages with Huffman
  * coding.
@@ -6,18 +8,21 @@
  */
 public class Driver {
     public static void main(String[] args) {
-        String[] messages = { "create a huffman tree", "huffman tree", "I can't believe this works...!" };
+        String[] messages = { "create a huffman tree", "huffman tree", "I can't believe this works...!",
+                "better tree" };
 
         for (String message : messages) {
             // Encode the original message
             HuffmanPair huffmanPair = Huffman.encode(message);
             HuffmanTree huffmanTree = huffmanPair.getHuffmanTree();
             String encodedMessage = huffmanPair.getEncodedMessage();
+            Map<Character, Integer> frequencyMap = Huffman.frequencyMap(message);
 
             // Decode the encoded message
             String decodedMessage = Huffman.decode(huffmanPair);
 
             System.out.println("Original Message:\t\t" + message);
+            System.out.println("Frequency Map:\t\t\t" + frequencyMap);
             System.out.println("Huffman Encoding Map:\t\t" + huffmanTree);
             System.out.println("Huffman Encoded Message:\t" + encodedMessage);
             System.out.println("Huffman Decoded Message:\t" + decodedMessage + "\n");
